ac4b04e74cc03aa653cdf2006a8879fc66b632aa
John Goerzen <jgoerzen@complete.org>
2006-06-30 03:34:46 +0100

    Added dh_make files

---
+++ debian/cron.d.ex
@@ -0,0 +1,4 @@
+#
+# Regular cron jobs for the hpodder package
+#
+0 4	* * *	root	hpodder_maintenance
---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(hpodder):needs="X11|text|vc|wm" section="Apps/see-menu-manual"\
+  title="hpodder" command="/usr/bin/hpodder"
