e7bc315b1dd9daefc319507163f04dbfc6bc96fc
John Goerzen <jgoerzen@complete.org>
2007-08-01 21:25:31 +0100

    Fix removeFile error

--- Commands/Download.hs
+++ Commands/Download.hs
@@ -88,7 +88,8 @@ cleanupDirectory gi episodes =
           epmsgs = map (\e -> e ++ ".msg") epmd5s
           eps = epmd5s ++ epmsgs
           removeold base file =
-            when (not (file `elem` eps)) $
+            when (not (file `elem` eps)) &&
+                 (not (file `elem` [".", ".."])) $
                 removeFile (base ++ "/" ++ file)
 
 downloadEpisodes gi episodes =
