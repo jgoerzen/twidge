cee314a5d30b30887d81be176791ee0768cbd54d
John Goerzen <jgoerzen@complete.org>
2008-02-07 07:54:54 +0100

    Run file in brief (-b) mode

--- Config.hs
+++ Config.hs
@@ -73,7 +73,7 @@ getDefaultCP =
                  cp <- set cp "DEFAULT" "epfailattempts" "15"
                  cp <- set cp "DEFAULT" "renametypes" "audio/mpeg:.mp3,audio/mp3:.mp3,x-audio/mp3:.mp3"
                  cp <- set cp "DEFAULT" "postproctypes" "audio/mpeg,audio/mp3,x-audio/mp3"
-                 cp <- set cp "DEFAULT" "gettypecommand" "file -i \"${EPFILENAME}\""
+                 cp <- set cp "DEFAULT" "gettypecommand" "file -b -i \"${EPFILENAME}\""
                  cp <- set cp "DEFAULT" "postproccommand" "id3v2 -A \"${CASTTITLE}\" -t \"${EPTITLE}\" --WOAF \"${EPURL}\" --WOAS \"${FEDDURL}\" \"${EPFILENAME}\""
                  return cp
 
