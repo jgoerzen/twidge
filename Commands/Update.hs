2f084c0e55ba2724e67cb7cbd3c792ce7e72ac74
John Goerzen <jgoerzen@complete.org>
2008-09-11 02:13:37 -0500

    Support for updates from stdin

--- Commands/Update.hs
+++ Commands/Update.hs
@@ -23,9 +23,10 @@ import Types
 import Text.Printf
 import System.Console.GetOpt
 import Data.List
-import Text.XML.HaXml
+import Text.XML.HaXml hiding (when)
 import Download
 import FeedParser
+import Control.Monad(when)
 
 i = infoM "update"
 
@@ -33,18 +34,25 @@ update = simpleCmd "update" "Update your status"
              update_help
              [] update_worker
 
+update_worker x cp ([], []) =
+    do l <- getLine
+       update_worker x cp ([], [l])
 update_worker _ cp ([], [status]) =
-    do xmlstr <- sendAuthRequest cp "/statuses/update.xml" [] 
+    do when (length status > 140)
+            (permFail $ "Your status update was " ++ show (length status) ++
+                      " characters; max length 140")
+       xmlstr <- sendAuthRequest cp "/statuses/update.xml" [] 
                  [("status", status)]
        debugM "update" $ "Got doc: " ++ xmlstr
-       -- let doc = getContent . xmlParse "follow" . stripUnicodeBOM $ xmlstr
-       -- return ()
        
 update_worker _ _ _ =
     permFail "update: syntax error; see twidge update --help"
 
 update_help =
- "Usage: twidge update status\n\n\
+ "Usage: twidge update [status]\n\n\
  \Updates your status to the given status.  You will most likely need to\n\
  \quote this to prevent interference from the shell.  For instance:\n\n\
- \  twidge update \"At home, baking.\"\n"
+ \  twidge update \"At home, baking.\"\n\n\
+ \You can also omit the status, in which case a single line will be read\n\
+ \from stdin and taken as your update.  Example:\n\n\
+ \  date | twidge update\n"
