2c04bb584fcc7b211b3163fc0a2f0daa791d0fda
John Goerzen <jgoerzen@complete.org>
2010-05-28 00:37:09 -0500

    Adding oauth bits to config

--- Config.hs
+++ Config.hs
@@ -1,5 +1,5 @@
 {- hpodder component
-Copyright (C) 2006-2008 John Goerzen <jgoerzen@complete.org>
+Copyright (C) 2006-2010 John Goerzen <jgoerzen@complete.org>
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -18,7 +18,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 {- |
    Module     : Config
-   Copyright  : Copyright (C) 2006-2008 John Goerzen
+   Copyright  : Copyright (C) 2006-2010 John Goerzen
    License    : GNU GPL, version 2 or above
 
    Maintainer : John Goerzen <jgoerzen@complete.org>
@@ -40,6 +40,9 @@ import System.Posix.Files(rename, setFileCreationMask)
 getDefaultCP =
     do return $ forceEither $ 
               do cp <- set startingcp "DEFAULT" "urlbase" "https://twitter.com"
+                 cp <- set startingcp "DEFAULT" "oauthrequesttoken" "%(urlbase)s/oauth/request_token"
+                 cp <- set startingcp "DEFAULT" "oauthaccesstoken" "%(urlbase)s/oauth/access_token"
+                 cp <- set startingcp "DEFAULT" "oauthauthorize" "%(urlbase)s/oauth/authorize"
                  cp <- set cp "DEFAULT" "sendmail" "/usr/sbin/sendmail"
                  cp <- set cp "DEFAULT" "shortenurls" "yes"
                  return cp
