jgoerzen/twidge

commit 8c4a9a0e8c5b24e52796d8b7efe010df6cc7565c

Added debugging and work around identica compatibility issue

Commands/Setup.hs
@@ -65,10 +65,18 @@ setup_worker cpath cp _ =
      let accUrl = fromJust . parseURL $ accUrlBase
      let authUrl = ((authUrlBase ++ "?oauth_token=") ++ ) . 
                    findWithDefault ("oauth_token", "") .
-          oauthParams
+                   oauthParams
      
      let CurlM resp = runOAuth $ do ignite app
-                                    oauthRequest HMACSHA1 Nothing reqUrl
+                                    
+                                    -- hack around hoauth bug - identica doesn't
+                                    -- return oauth_callback_confirmed
+                                    putToken $ AccessToken {application = app,
+                                                            oauthParams = empty}
+                                    reqres <- oauthRequest HMACSHA1 Nothing reqUrl
+                                    liftIO $ d $ "reqres params: " ++ case reqres of 
+                                      Left x -> " error " ++ x
+                                      Right y -> show (oauthParams y)
                                     twidgeAskAuthorization authUrl
                                     oauthRequest HMACSHA1 Nothing accUrl
                                     tok <- getToken

commit e3d167234181a780cdd4adde1682cd6f8dadb94f

Fix oAuth on twitter
identi.ca support in 8c4a9a0e8c5b24e52796d8b7efe010df6cc7565c broke it

Closes #8

Commands/Setup.hs
@@ -71,8 +71,8 @@ setup_worker cpath cp _ =
                                     
                                     -- hack around hoauth bug - identica doesn't
                                     -- return oauth_callback_confirmed
-                                    putToken $ AccessToken {application = app,
-                                                            oauthParams = empty}
+                                    -- putToken $ AccessToken {application = app,
+                                    --                        oauthParams = empty}
                                     reqres <- oauthRequest HMACSHA1 Nothing reqUrl
                                     liftIO $ d $ "reqres params: " ++ case reqres of 
                                       Left x -> " error " ++ x